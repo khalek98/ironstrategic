bca97ed0f598d38b14a9f9ef95f09b92beb5e120
Khalek <xalek.islam@gmail.com>
2022-11-12 02:24:05 +0200

    update video type

--- gulpfile.js
+++ gulpfile.js
@@ -32,9 +32,11 @@ gulp.task('watch', function () {
   gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
   gulp.watch('src/*.html').on('change', gulp.parallel('html'));
   // gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
+
   gulp.watch('src/fonts/**/*').on('all', gulp.parallel('fonts'));
   gulp.watch('src/icons/**/*').on('all', gulp.parallel('icons'));
   gulp.watch('src/img/**/*').on('all', gulp.parallel('images'));
+  gulp.watch('src/source/**/*').on('all', gulp.parallel('sources'));
 });
 
 gulp.task('html', function () {
@@ -66,7 +68,15 @@ gulp.task('images', function () {
     .pipe(browserSync.stream());
 });
 
+gulp.task('sources', function () {
+  return gulp
+    .src('src/source/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/source'))
+    .pipe(browserSync.stream());
+});
+
 gulp.task(
   'default',
-  gulp.parallel('watch', 'server', 'styles', 'fonts', 'icons', 'html', 'images'),
+  gulp.parallel('watch', 'server', 'styles', 'sources', 'fonts', 'icons', 'html', 'images'),
 );
