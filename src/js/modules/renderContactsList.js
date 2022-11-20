d1ec17dc6af8486801e62ac8e6c645c236f4b6c6
Khalek <xalek.islam@gmail.com>
2022-11-20 20:44:24 +0200

    map on width: 576px+; add list active elem

--- src/js/modules/renderContactsList.js
+++ src/js/modules/renderContactsList.js
@@ -12,27 +12,33 @@ const renderContactsList = (list) => {
   });
 
   const points = [];
+  const elements = [];
   let activePoint;
 
-  const setContinent = (e, item, point) => {
+  const setContinent = (e, item, point, elem) => {
     if (e.code && e.code !== 'Enter') return;
     selectedText.innerHTML = item.continent;
     mailText.innerText = item.mail;
     mailText.href = `mailto:${item.mail}`;
-    if (point) {
-      points.forEach((pointItem, i) => {
-        pointItem.classList.remove('footer-map__point_active');
-        pointItem.style.cssText = `
+
+    points.forEach((pointItem, i) => {
+      pointItem.classList.remove('footer-map__point_active');
+      pointItem.style.cssText = `
           top: ${list[i].location.top}px;
           left: ${list[i].location.left}px;
         `;
-      });
-      point.classList.add('footer-map__point_active');
-      point.style.cssText = `
+    });
+    point.classList.add('footer-map__point_active');
+    point.style.cssText = `
       top: ${item.location.top - 11}px;
       left: ${item.location.left - 6}px;
     `;
-    }
+
+    elements.forEach((elemItem) => {
+      elemItem.classList.remove('footer-block__item_active');
+    });
+    elem.classList.add('footer-block__item_active');
+
     activePoint = point;
     closeList(contactsList);
   };
@@ -43,8 +49,8 @@ const renderContactsList = (list) => {
     elem.innerHTML = item.continent;
     elem.classList.add('footer-block__item');
     elem.setAttribute('tabindex', 0);
-    elem.addEventListener('click', (e) => setContinent(e, item, point));
-    elem.addEventListener('keydown', (e) => setContinent(e, item, point));
+    elem.addEventListener('click', (e) => setContinent(e, item, point, elem));
+    elem.addEventListener('keydown', (e) => setContinent(e, item, point, elem));
 
     point.addEventListener('mouseenter', () => {
       point.classList.add('footer-map__point', 'footer-map__point_active');
@@ -63,11 +69,13 @@ const renderContactsList = (list) => {
     });
 
     if (i === 0) {
+      elem.classList.add('footer-block__item_active');
       point.classList.add('footer-map__point', 'footer-map__point_active');
       point.style.cssText = `
       top: ${item.location.top - 11}px;
       left: ${item.location.left - 6}px;
     `;
+      activePoint = point;
     } else {
       point.classList.add('footer-map__point');
       point.style.cssText = `
@@ -77,10 +85,11 @@ const renderContactsList = (list) => {
     }
 
     point.addEventListener('click', (e) => {
-      setContinent(e, item, point);
+      setContinent(e, item, point, elem);
     });
 
     contactsList.appendChild(elem);
+    elements.push(elem);
     map.appendChild(point);
     points.push(point);
   });
--- src/js/modules/toggleContactsList.js
+++ src/js/modules/toggleContactsList.js
@@ -1,19 +1,24 @@
 export const closeList = (modal) => {
-  modal.style.display = 'none';
+  // modal.style.display = 'none';
+  setTimeout(() => (modal.style.display = 'none'), 400);
+  modal.style.animation = '0.4s ease-in-out 0s 1 normal none running closeList';
+  // setTimeout(() => modal.style.animation)
 };
 
-export const openList = (trigger, modal, e) => {
+export const openList = (modal, e) => {
   if (e.code && e.code !== 'Enter') return;
   if (modal.style.display === 'grid') {
-    modal.style.display = 'none';
+    setTimeout(() => (modal.style.display = 'none'), 400);
+    modal.style.animation = '0.4s ease-in-out 0s 1 normal none running closeList';
   } else {
     modal.style.display = 'grid';
+    modal.style.animation = '0.4s ease-in-out 0s 1 normal none running openList';
   }
 };
 
 const openByEvent = (trigger, modal, action) => {
   // console.log(trigger, modal, action);
-  trigger.addEventListener(action, (e) => openList(trigger, modal, e));
+  trigger.addEventListener(action, (e) => openList(modal, e));
 };
 
 const toggleContactsList = () => {
