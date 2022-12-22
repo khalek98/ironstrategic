b3cc992dc4859814b2b4d6e111b2de9768cf2aa7
Khalek <xalek.islam@gmail.com>
2022-12-22 15:13:52 +0200

    anim. logo, arrow, fix bug scale map

--- src/js/modules/renderContactsList.js
+++ src/js/modules/renderContactsList.js
@@ -1,8 +1,9 @@
-import { closeList } from './toggleContactsList';
+import { closeList } from "./toggleContactsList";
 
 const transformMapScale = (map, e) => {
   const viewWidth =
-    e?.target?.outerWidth || +window.getComputedStyle(document.body).width.replace(/\D/g, '');
+    e?.target?.currentTarget?.innerWidth ||
+    +window.getComputedStyle(document.body).width.replace(/\D/g, "");
 
   if (viewWidth > 768 && viewWidth < 991) {
     map.style.transform = `
@@ -10,7 +11,7 @@ const transformMapScale = (map, e) => {
      scale(${(viewWidth / 100) * 0.12})
     `;
   } else if (viewWidth >= 1200) {
-    map.style.transform = 'none';
+    map.style.transform = "none";
   } else if (viewWidth > 991 && viewWidth < 1200) {
     map.style.transform = `
     translateX(-30%)
@@ -25,63 +26,63 @@ const transformMapScale = (map, e) => {
 };
 
 const renderContactsList = (list) => {
-  const contactsList = document.querySelector('.footer-block__list'),
-    selectedText = document.querySelector('.footer-block__selected span'),
-    mailElem = document.querySelector('.footer-block__email'),
-    mailText = document.querySelector('.footer-block__email span'),
-    map = document.querySelector('.footer-map');
+  const contactsList = document.querySelector(".footer-block__list"),
+    selectedText = document.querySelector(".footer-block__selected span"),
+    mailElem = document.querySelector(".footer-block__email"),
+    mailText = document.querySelector(".footer-block__email span"),
+    map = document.querySelector(".footer-map");
 
   const points = [];
   const elements = [];
   let activePoint;
 
   const setContinent = (e, item, point, elem) => {
-    if (e.code && e.code !== 'Enter') return;
+    if (e.code && e.code !== "Enter") return;
     selectedText.innerHTML = item.continent;
     mailText.innerText = item.mail;
     mailText.href = `mailto:${item.mail}`;
 
     points.forEach((pointItem, i) => {
-      pointItem.classList.remove('footer-map__point_active');
+      pointItem.classList.remove("footer-map__point_active");
       pointItem.style.cssText = `
           top: ${list[i].location.top}px;
           left: ${list[i].location.left}px;
         `;
     });
-    point.classList.add('footer-map__point_active');
+    point.classList.add("footer-map__point_active");
     point.style.cssText = `
       top: ${item.location.top - 11}px;
       left: ${item.location.left - 6}px;
     `;
 
     elements.forEach((elemItem) => {
-      elemItem.classList.remove('footer-block__item_active');
+      elemItem.classList.remove("footer-block__item_active");
     });
-    elem.classList.add('footer-block__item_active');
+    elem.classList.add("footer-block__item_active");
 
     activePoint = point;
     closeList(contactsList);
   };
 
   list.forEach((item, i) => {
-    const elem = document.createElement('li');
-    const point = document.createElement('div');
+    const elem = document.createElement("li");
+    const point = document.createElement("div");
     elem.innerHTML = item.continent;
-    elem.classList.add('footer-block__item');
-    elem.setAttribute('tabindex', 0);
-    elem.addEventListener('click', (e) => setContinent(e, item, point, elem));
-    elem.addEventListener('keydown', (e) => setContinent(e, item, point, elem));
+    elem.classList.add("footer-block__item");
+    elem.setAttribute("tabindex", 0);
+    elem.addEventListener("click", (e) => setContinent(e, item, point, elem));
+    elem.addEventListener("keydown", (e) => setContinent(e, item, point, elem));
 
-    point.addEventListener('mouseenter', () => {
-      point.classList.add('footer-map__point', 'footer-map__point_active');
+    point.addEventListener("mouseenter", () => {
+      point.classList.add("footer-map__point", "footer-map__point_active");
       point.style.cssText = `
       top: ${item.location.top - 11}px;
       left: ${item.location.left - 6}px;
     `;
     });
-    point.addEventListener('mouseleave', () => {
+    point.addEventListener("mouseleave", () => {
       if (activePoint === point) return;
-      point.classList.remove('footer-map__point_active');
+      point.classList.remove("footer-map__point_active");
       point.style.cssText = `
       top: ${item.location.top}px;
       left: ${item.location.left}px;
@@ -89,22 +90,22 @@ const renderContactsList = (list) => {
     });
 
     if (i === 0) {
-      elem.classList.add('footer-block__item_active');
-      point.classList.add('footer-map__point', 'footer-map__point_active');
+      elem.classList.add("footer-block__item_active");
+      point.classList.add("footer-map__point", "footer-map__point_active");
       point.style.cssText = `
       top: ${item.location.top - 11}px;
       left: ${item.location.left - 6}px;
     `;
       activePoint = point;
     } else {
-      point.classList.add('footer-map__point');
+      point.classList.add("footer-map__point");
       point.style.cssText = `
       top: ${item.location.top}px;
       left: ${item.location.left}px;
     `;
     }
 
-    point.addEventListener('click', (e) => {
+    point.addEventListener("click", (e) => {
       setContinent(e, item, point, elem);
     });
 
@@ -114,8 +115,8 @@ const renderContactsList = (list) => {
     points.push(point);
   });
 
-  mailElem.addEventListener('click', () => navigator.clipboard.writeText(mailText.innerText));
-  window.addEventListener('resize', (e) => transformMapScale(map, e));
+  mailElem.addEventListener("click", () => navigator.clipboard.writeText(mailText.innerText));
+  window.addEventListener("resize", (e) => transformMapScale(map, e));
 
   transformMapScale(map);
 };
--- src/js/modules/toggleContactsList.js
+++ src/js/modules/toggleContactsList.js
@@ -1,31 +1,28 @@
 export const closeList = (modal) => {
-  // modal.style.display = 'none';
-  setTimeout(() => (modal.style.display = 'none'), 400);
-  modal.style.animation = '0.4s ease-in-out 0s 1 normal none running closeList';
-  // setTimeout(() => modal.style.animation)
+  setTimeout(() => (modal.style.display = "none"), 400);
+  modal.style.animation = "0.4s ease-in-out 0s 1 normal none running closeList";
 };
 
 export const openList = (modal, e) => {
-  if (e.code && e.code !== 'Enter') return;
-  if (modal.style.display === 'grid') {
-    setTimeout(() => (modal.style.display = 'none'), 400);
-    modal.style.animation = '0.4s ease-in-out 0s 1 normal none running closeList';
+  if (e.code && e.code !== "Enter") return;
+  if (modal.style.display === "grid") {
+    setTimeout(() => (modal.style.display = "none"), 400);
+    modal.style.animation = "0.4s ease-in-out 0s 1 normal none running closeList";
   } else {
-    modal.style.display = 'grid';
-    modal.style.animation = '0.4s ease-in-out 0s 1 normal none running openList';
+    modal.style.display = "grid";
+    modal.style.animation = "0.4s ease-in-out 0s 1 normal none running openList";
   }
 };
 
 const openByEvent = (trigger, modal, action) => {
-  // console.log(trigger, modal, action);
   trigger.addEventListener(action, (e) => openList(modal, e));
 };
 
 const toggleContactsList = () => {
-  const selectedBlock = document.querySelector('.footer-block__selected'),
-    contactsList = document.querySelector('.footer-block__list');
+  const selectedBlock = document.querySelector(".footer-block__selected"),
+    contactsList = document.querySelector(".footer-block__list");
 
-  document.body.addEventListener('click', (e) => {
+  document.body.addEventListener("click", (e) => {
     if (
       e.target !== contactsList &&
       e.target !== selectedBlock &&
@@ -35,13 +32,13 @@ const toggleContactsList = () => {
     }
   });
 
-  document.body.addEventListener('keydown', (e) => {
-    if (e.code === 'Escape') closeList(contactsList);
+  document.body.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") closeList(contactsList);
   });
 
   // openList(contactsList);
-  openByEvent(selectedBlock, contactsList, 'click');
-  openByEvent(selectedBlock, contactsList, 'keydown');
+  openByEvent(selectedBlock, contactsList, "click");
+  openByEvent(selectedBlock, contactsList, "keydown");
 };
 
 export default toggleContactsList;
